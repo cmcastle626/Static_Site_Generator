bf2473474f04b1981412d59a55cc7fdebe67348d
cmcastle626 <cmcastle626@gmail.com>
2026-06-13 17:48:49 -0700

    fix main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    text_object = TextNode("This is some anchor text", TextType("**Bold text**"), "https://www.boot.dev")
+    text_object = TextNode("This is some anchor text", TextType("bold"), "https://www.boot.dev")
     print(f"{text_object}")
 
 main()
