00e194903e54e35e79406a7d107c61c2261d6b4b
cmcastle626 <cmcastle626@gmail.com>
2026-06-17 18:49:51 -0700

    added text node to html node fn and tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     text_texttype = "text"
@@ -21,3 +22,22 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    ttype = text_node.text_type
+    print(f"proccing ttype check... ttype: {ttype}")
+    if ttype == TextType.text_texttype:
+        return LeafNode(None, text_node.text, None)
+    elif ttype == TextType.bold_texttype:
+        return LeafNode("b", text_node.text)
+    elif ttype == TextType.italic_texttype:
+        return LeafNode("i", text_node.text)
+    elif ttype == TextType.code_texttype:
+        return LeafNode("code", text_node.text)
+    elif ttype == TextType.link_texttype:
+        return LeafNode("a", text_node.text,{"href":text_node.url})
+    elif ttype == TextType.image_texttype:
+        return LeafNode("img","",{"src":text_node.url, "alt":text_node.text})
+    else:
+        raise Exception("not using supported text type")
+    
